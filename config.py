af074939f3d34ee15abbcee0edd499577540cf45
misaka7690 <bilibili@lris.net>
2020-03-16 16:29:45 +0800

    📝 add some comment

--- config.py
+++ config.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-  
-family_name = ""
-m_uid = ""
-m_password = ""
-m_qq_pwd = ""
+""" 
+family_name: 你的同居住的家人的名字,由于只填一个就行了,我这里只写了一个,两个以上去源码增加
+m_uid: 学号,类似U201714***
+m_password: hust认证密码
+m_qq_pwd: QQ授权码
+m_sender_email: 发送的QQ邮箱
+m_receiver_email: 接受邮件的邮箱
+"""
+family_name = "" 
 
-m_sender_email = ""
-m_receiver_email = ""
+m_uid = ""  # 
+m_password = "" 
+m_qq_pwd = "" # # QQ授权码
+
+m_sender_email = "" # 发送的QQ邮箱
+m_receiver_email = "" # 接受邮件的邮箱
